95ab94d5b705b3e13ce3b6b8bfe3a905f26f1412
fabricio.agsf@gmail.com <fabricio.agsf@gmail.com>
2021-01-09 12:37:47 -0300

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -112,6 +112,15 @@ public class Order implements Serializable{
 	}
 
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for (Product p : products) {
+			sum += p.getPrice();
+		}
+		return sum;
+	}
+	
+	
 	public Set<Product> getProducts() {
 		return products;
 	}
@@ -144,7 +153,4 @@ public class Order implements Serializable{
 	}
 
 
-
-	
-	
 }
