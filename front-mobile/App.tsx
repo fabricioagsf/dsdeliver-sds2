795d3e86ca6067f22715b7b54b5800f6009b8ba6
fabricio.agsf@gmail.com <fabricio.agsf@gmail.com>
2021-01-09 16:44:02 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,10 +1,9 @@
+import React from 'react';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import { StatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header';
-import Home from './src/Home';
+import { StyleSheet,  View } from 'react-native';
+import { Routes } from './src/Routes';
 
 
 export default function App() {
@@ -20,8 +19,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Header />
-      <Home />
+      <Routes />
       <StatusBar style="auto" />
     </View>
   );
